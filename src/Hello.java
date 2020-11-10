117b36f8c286d4a708998eb27788d07374d99500
Ariuntungalag Otgonbayar <ariukaa_mn2005@yahoo.com>
2020-11-10 11:49:16 -0600

    Project is already in remote repo, committing more changes

--- src/Hello.java
+++ src/Hello.java
@@ -2,4 +2,10 @@ public class Hello {
     // more stuff
     //mac users and windows users are happy to see commit dialog finally
 
+    //I have already published on GitHub.com
+    //now I am ready to make more commits
+    //but do I need to share again?
+    //ABSOLUTELY NO
+    // ITS TIME TO COMMIT AND JUST PUSH!!!!
+
 }
